7e691cece4b009dfd553d52af6c8a6f07d1d7a97
marsipu <46245704+marsipu@users.noreply.github.com>
2020-10-06 14:57:02 +0200

    First working LoadCell-Trigger-Algorythm

--- trigger_funcs.py
+++ trigger_funcs.py
@@ -4,40 +4,90 @@ import matplotlib.pyplot as plt
 import time
 
 
-def get_trigger_only_meg(sub):
-    raw = sub.load_raw()
-
+def _get_trig_ch(raw):
     if raw.info['nchan'] > 130:
-        trig_ch = ['EEG 029']
+        trig_ch = 'EEG 029'
     elif raw.info['nchan'] == 1:
         trig_ch = None
     else:
-        trig_ch = ['EEG 001']
+        trig_ch = 'EEG 001'
 
-    pd_data = raw.to_data_frame(picks=['EEG 001'])
-    eeg_series = pd_data['EEG 001']
+    return trig_ch
 
+
+def _get_load_cell_trigger(raw):
+    trig_ch = _get_trig_ch(raw)
+    pd_data = raw.to_data_frame(picks=trig_ch)
+    eeg_series = pd_data[trig_ch]
+
+    # Difference of Rolling Mean on both sides of each value, window=2000
     rolling_left2000 = eeg_series.rolling(2000, min_periods=1).mean()
     rolling_right2000 = eeg_series.iloc[::-1].rolling(2000, min_periods=1).mean()
     rolling_diff2000 = rolling_left2000 - rolling_right2000
 
+    # Difference of Rolling Mean on both sides of each value, window=100
     rolling_left100 = eeg_series.rolling(100, min_periods=1).mean()
     rolling_right100 = eeg_series.iloc[::-1].rolling(100, min_periods=1).mean()
     rolling_diff100 = rolling_left100 - rolling_right100
 
+    # Difference of Rolling Standard-Deviation on both sides of each value, window=200
+    rolling_leftstd200 = eeg_series.rolling(200, min_periods=1).std()
+    rolling_rightstd200 = eeg_series.iloc[::-1].rolling(200, min_periods=1).std()
+    rolling_diffstd200 = rolling_leftstd200 - rolling_rightstd200
+
+    # Difference of Rolling Standard-Deviation on both sides of each value, window=100
     rolling_leftstd100 = eeg_series.rolling(100, min_periods=1).std()
     rolling_rightstd100 = eeg_series.iloc[::-1].rolling(100, min_periods=1).std()
     rolling_diffstd100 = rolling_leftstd100 - rolling_rightstd100
 
-    events = np.hstack((np.expand_dims(trig_peaks, axis=1), np.full((len(trig_peaks), 2), 0)))
+    std2000 = np.std(rolling_diff2000)
+    stdstd100 = np.std(rolling_diffstd100) * 2
 
-    for n, event in enumerate(events):
-        if trig_diff[event[0]] < 0:
-            events[n, 2] = 1
-        elif trig_diff[event[0]] > 0:
-            events[n, 2] = 2
+    rd2000_peaks, rd2000_props = find_peaks(abs(rolling_diff2000), height=std2000, distance=3000)
+    rd100_peaks, rd100_props = find_peaks(abs(rolling_diff100), distance=100)
+    rdstd200_peaks, rdstd200_props = find_peaks(abs(rolling_diffstd200), distance=100)
+    rdstd100_peaks, rdstd100_props = find_peaks(abs(rolling_diffstd100), height=stdstd100, distance=100)
+
+    return pd_data, rolling_diff2000, rolling_diff100, rolling_diffstd200, rolling_diffstd100, \
+           rd2000_peaks, rd100_peaks, rdstd200_peaks, rdstd100_peaks, std2000, stdstd100
+
+
+def get_load_cell_events(sub):
+    raw = sub.load_raw()
+
+    pd_data, rolling_diff2000, rolling_diff100, rolling_diffstd200, rolling_diffstd100, \
+    rd2000_peaks, rd100_peaks, rdstd200_peaks, rdstd100_peaks, std2000, stdstd100 = _get_load_cell_trigger(raw)
+
+    events = np.ndarray((0, 3), dtype='int')
+    for pk in rd2000_peaks:
+
+        # get the two closest peaks of rdstd100 to the rd2000-peak
+        distance_rdstd100 = rdstd100_peaks - pk
+        closest_rdstd100_peak_idxs = abs(distance_rdstd100).argsort()[:2]
+
+        if rolling_diff2000[pk] > 0:
+            events = np.append(events, [[pk, 0, 2]], axis=0)
+            for std_pkx in closest_rdstd100_peak_idxs:
+                std_pk = rdstd100_peaks[std_pkx]
+                if distance_rdstd100[std_pkx] < 0:
+                    events = np.append(events, [[std_pk, 0, 1]], axis=0)
+                else:
+                    events = np.append(events, [[std_pk, 0, 3]], axis=0)
+        else:
+            events = np.append(events, [[pk, 0, 5]], axis=0)
+            for std_pkx in closest_rdstd100_peak_idxs:
+                std_pk = rdstd100_peaks[std_pkx]
+                if distance_rdstd100[std_pkx] < 0:
+                    events = np.append(events, [[std_pk, 0, 4]], axis=0)
+                else:
+                    events = np.append(events, [[std_pk, 0, 6]], axis=0)
 
     print(f'{len(events)} events found for {sub.name}')
+
+    # sort events
+    events = events[events[:, 0].argsort()]
+
+    events[:, 0] += raw.first_samp
 
     sub.save_events(events)
 
@@ -46,47 +96,43 @@ def plot_part_trigger(sub):
     raw = sub.load_raw()
 
     # raw = raw.filter(0, 20, n_jobs=-1)
+    trig_ch = _get_trig_ch(raw)
 
-    pd_data = raw.to_data_frame(picks=['EEG 001'])
-    eeg_series = pd_data['EEG 001']
+    pd_data, rolling_diff2000, rolling_diff100, rolling_diffstd200, rolling_diffstd100, \
+    rd2000_peaks, rd100_peaks, rdstd200_peaks, rdstd100_peaks, std2000, stdstd100 = _get_load_cell_trigger(raw)
 
-    rolling_left2000 = eeg_series.rolling(2000, min_periods=1).mean()
-    rolling_right2000 = eeg_series.iloc[::-1].rolling(2000, min_periods=1).mean()
-    rolling_diff2000 = rolling_left2000 - rolling_right2000
-    rd2000_peaks = find_peaks(rolling_diff2000)
-
-    rolling_left100 = eeg_series.rolling(100, min_periods=1).mean()
-    rolling_right100 = eeg_series.iloc[::-1].rolling(100, min_periods=1).mean()
-    rolling_diff100 = rolling_left100 - rolling_right100
-    rd100_peaks = find_peaks(rolling_diff100)
-
-    rolling_leftstd200 = eeg_series.rolling(200, min_periods=1).std()
-    rolling_rightstd200 = eeg_series.iloc[::-1].rolling(200, min_periods=1).std()
-    rolling_diffstd200 = rolling_leftstd200 - rolling_rightstd200
-    rdstd200_peaks = find_peaks()
-
-    rolling_leftstd100 = eeg_series.rolling(100, min_periods=1).std()
-    rolling_rightstd100 = eeg_series.iloc[::-1].rolling(100, min_periods=1).std()
-    rolling_diffstd100 = rolling_leftstd100 - rolling_rightstd100
+    tmin = 80000
+    tmax = 120000
 
     plt.figure()
-    plt.plot(eeg_series[160000:200000], label='data')
-    plt.plot(rolling_diff2000[160000:200000], label='RollingDiff2000')
-    plt.plot(rolling_diff100[160000:200000], label='RollingDiff100')
-    plt.plot(rolling_diffstd200[160000:200000], label='RollingDiffStd200')
-    plt.plot(rolling_diffstd100[160000:200000], label='RollingDiffStd100')
+    plt.plot(pd_data[trig_ch][tmin:tmax], label='data')
+    plt.plot(rolling_diff2000[tmin:tmax], label='RollingDiff2000')
+    plt.plot(rolling_diff100[tmin:tmax], label='RollingDiff100')
+    plt.plot(rolling_diffstd200[tmin:tmax], label='RollingDiffStd200')
+    plt.plot(rolling_diffstd100[tmin:tmax], label='RollingDiffStd100')
+    plt.plot([p for p in rd2000_peaks if tmin < p < tmax],
+             [rolling_diff2000[p] for p in rd2000_peaks if tmin < p < tmax], 'x', label='RollingDiff2000-Peaks')
+    plt.plot(range(tmin, tmax), np.full(tmax-tmin, std2000), label='RollingDiff2000-Std')
+    plt.plot(range(tmin, tmax), np.full(tmax-tmin, stdstd100), label='RollingDiffStd100-Std')
+    plt.plot([p for p in rd100_peaks if tmin < p < tmax],
+             [rolling_diff100[p] for p in rd100_peaks if tmin < p < tmax], 'x', label='RollingDiff100-Peaks')
+    plt.plot([p for p in rdstd200_peaks if tmin < p < tmax],
+             [rolling_diffstd200[p] for p in rdstd200_peaks if tmin < p < tmax], 'x',
+             label='RollingDiffStd200-Peaks')
+    plt.plot([p for p in rdstd100_peaks if tmin < p < tmax],
+             [rolling_diffstd100[p] for p in rdstd100_peaks if tmin < p < tmax], 'x',
+             label='RollingDiffStd100-Peaks')
     plt.legend()
     plt.show()
 
 
-def only_eeg(sub):
-    raw_filtered = sub.load_filtered()
-    if raw_filtered.info['nchan'] > 130:
-        trig_ch = ['EEG 029']
-    elif raw_filtered.info['nchan'] == 1:
-        trig_ch = None
-    else:
-        trig_ch = ['EEG 001']
-    raw_filtered = raw_filtered.pick(trig_ch)
-
-    sub.save_filtered(raw_filtered)
+def plot_load_cell_trigger(sub):
+    raw = sub.load_raw()
+    trig_ch = _get_trig_ch(raw)
+    eeg_raw = raw.pick(trig_ch)
+    try:
+        events = sub.load_events()
+    except FileNotFoundError:
+        get_load_cell_events(sub)
+        events = sub.load_events()
+    eeg_raw.plot(events, duration=30, scalings='auto', title=sub.name)
